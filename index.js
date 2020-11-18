8b440fd1b327f466655b82c6022fffed1da715b3
Gianlorenzo Urbano <68033078+gianlourbano@users.noreply.github.com>
2020-11-18 14:00:42 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require("express")
+const cors = require("cors")
+const puppeteer = require("puppeteer")
+require("dotenv").config()
+var cron = require("node-cron")
+const axios = require("axios").default
+
+const app = express()
+
+app.use(cors())
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+const PORT = process.env.PORT || 3000
+
+// Database initialization
+const db = require("./database/initializeDb")
+const dbName = "Covid19"
+db.initialize(app, dbName, "data")
+
+//Express server
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`)
+})
+
+const primaryID = process.env.PRIMARY_ID
+const secondaryID = process.env.SECONDARY_ID
+const tertiaryID = process.env.TERTIARY_ID
+
+const Scrape = async () => {
+    const browser = await puppeteer.launch({ headless: true })
+    const page = await browser.newPage()
+
+    await page.goto(process.env.URL)
+
+    const data = await page.evaluate(() => {
+        const tds = Array.from(document.querySelectorAll("table tr td"))
+        return tds.map(td => td.innerText)
+    });
+
+    await browser.close()
+
+    console.log(data)
+    const world_data = data.slice(1, 9)
+
+    const date_ob = new Date()
+
+    let date = ("0" + date_ob.getDate()).slice(-2)
+    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2)
+    let year = date_ob.getFullYear()
+    let hours = ("0" + date_ob.getHours()).slice(-2)
+    let minutes = ("0" + date_ob.getMinutes()).slice(-2)
+    let seconds = ("0" + date_ob.getSeconds()).slice(-2)
+
+    let time = `${date}-${month}-${year} (${hours}:${minutes}:${seconds})`
+
+    const obj = {
+        type: world_data[0],
+        total_cases: world_data[1],
+        new_cases: world_data[2],
+        total_deaths: world_data[3],
+        new_deaths: world_data[4],
+        total_recovered: world_data[5],
+        active_cases: world_data[6],
+        critical_active_cases: world_data[7],
+        time: time
+    }
+
+    axios.put(`http://localhost:${PORT}/api/data/${primaryID}`, obj)
+        .catch(error => console.log(error))
+}
+
+const SwapData = async (id1, id2) => {
+    const temp = await axios.get(`http://localhost:${PORT}/api/data/${id1}`)
+    await axios.put(`http://localhost:${PORT}/api/data/${id2}`, temp)
+        .catch(error => console.log(error))
+}
+
+//At midnight, the server saves the current data
+cron.schedule("0 0 * * *", async () => {
+    await SwapData(secondaryID, tertiaryID)
+    await SwapData(primaryID, secondaryID)
+})
+
+//The server scrapes every 30 minutes
+cron.schedule("*/30 * * * *", async () => {
+    await Scrape()
+    console.log("scraping in 30 minutes") 
+})
